7a2ab742feac4f905936cb26f16344ae46468622
Adam Otto <aotto@ualberta.ca>
2018-02-07 15:49:43 -0700

    Browser reads from a local csv file and displays markers on map.

--- ECE492GUI/scripts.js
+++ ECE492GUI/scripts.js
@@ -2,6 +2,16 @@
  * @author adamo
  */
 
+$(document).ready(function() {
+	$.ajax({	
+		type: "GET",	
+		url: "test.csv",	
+		dataType: "text",	
+		success: function(data) {csvFunction(data);}	
+	});
+});
+
+var csvData;
 var map;
 //window.onscroll = function() {stickyFunc()};
 var mapElement;
@@ -13,7 +23,8 @@ function init(){
 	window.onscroll = function() {stickyFunc()};
 	mapElement = document.getElementById("fixedDiv");
 	sticky = mapElement.offsetTop;
-	initContent();	
+	initMap();
+	initContent(csvData);
 	console.log("init end.");
 }
 
@@ -23,25 +34,70 @@ function initMap() {
 	    center:new google.maps.LatLng(53.5444,-113.4909),
 	    zoom:5,
 	};
-	var marker = new google.maps.Marker({position:center});
+	//var marker = new google.maps.Marker({position:center});
 	
-	map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 	
-	marker.setMap(map);
+	//marker.setMap(map);
 }
 
 
-function initContent() {
+function initContent(data) {
+	//Code help from -> https://code.tutsplus.com/tutorials/parsing-a-csv-file-with-javascript--cms-25626
 	console.log("initContent started...");
-	var htmlCode = "";
+	var htmlCode = "<table>";
+	var allRows = data.split(/\r?\n|\r/);
 	
-	for(var i = 0; i < 100; i++)
-	{		
-		htmlCode += "<p>Content goes here.</p>";
+	for (var row = 0; row < allRows.length; row++) {
+		if (row === 0) {
+			htmlCode += '<thead>';
+			htmlCode += '<tr>';
+		}
+		else {
+			htmlCode += '<tr>';
+		}
+		
+		var rowCells = allRows[row].split(',');
+		var latitude = 0;
+		var longitude = 0;
+		for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+			if (row === 0) {
+				htmlCode += '<th>';
+				htmlCode += rowCells[rowCell];
+				htmlCode += '</th>';
+			} else {
+				
+				if(rowCell == 1)
+					latitude = parseInt( rowCells[rowCell] );
+				if(rowCell == 2)
+					longitude = parseInt( rowCells[rowCell] );
+				htmlCode += '<td>';
+				htmlCode += rowCells[rowCell];
+				htmlCode += '</td>';
+			}
+		}
+		var pos = new google.maps.LatLng(latitude, longitude);
+		var mark = new google.maps.Marker({position:pos});
+		mark.setMap(map);
+		
+		if (row === 0) {
+			htmlCode += '</tr>';
+			htmlCode += '</thead>';
+			htmlCode += '<tbody>';
+		} else {
+		  htmlCode += '</tr>';
+		}
 	}
-	
+	htmlCode += '</tbody>';
+	htmlCode += '</table>';
 	document.getElementById('content').innerHTML = htmlCode;
 	console.log("initContent end.");
+}
+
+function csvFunction(data)
+{
+	console.log(data);
+	csvData = data;
 }
 
 function stickyFunc(){
