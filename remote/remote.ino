167e26727a0933ae723dac277288643726ad529f
hidalgok <kenchristopherhidalgo@gmail.com>
2018-02-12 15:02:45 -0700

    remote files compiling

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,31 +1,42 @@
-#include <GSM.h>
-#include <string.h>
-#include <DHT.h>
+#include "DHT.h"
 #include "SMPWM01A.h"
+#include "Adafruit_FONA.h"
+
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
 
 
 // Constants
 // DHT22 specific
-#define DHTPIN 2     // Temp & Humid Sensor
+#define DHTPIN 8     // Temp & Humid Sensor
 #define DHTTYPE DHT22   // DHT 22 signal pin
 #define HOME_PHONE "+1780555555" //Home node phone number
+#define PINNUMBER "" //SIM card PIN number
+
+#include "SoftwareSerial.h"
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
 DHT dht(DHTPIN, DHTTYPE); // Instantiate dht
 SMPWM01A dust; // Instantiate dust sensor
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  boolean notConnected = true;
-  while (notConnected) {
-    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
-      notConnected = false;
-    } else {
-      delay(1000);
-    }
+
+  fonaSerial->begin(4800);
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+    while(1);
   }
+
+  fonaSerial->print("AT+CNMI=2,1\r\n");
+  fona.enableGPS(true);
 }
 
 void loop() {
@@ -47,6 +58,15 @@ void loop() {
   char txtmsg[80];
 
   //Update sensor values here
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  dust25 = dust.getPM2();
+  dust100 = dust.getPM10();
+
+  if (fona.getNetworkStatus() == 1) {
+    // network & GPRS? Great! Print out the GSM location to compare
+    boolean gsmloc_success = fona.getGSMLoc(&lat, &lon);
+  }
   
   //convert floating point values to strings
   dtostrf(hum, 10, 4, h);
@@ -56,9 +76,6 @@ void loop() {
   dtostrf(lat, 10, 4, la);
   dtostrf(lon, 10, 4, lo);
   
-  sms.beginSMS(HOME_PHONE);
   snprintf(txtmsg, sizeof(txtmsg), "%s,%s,%s,%s,%s,%s", h,t,d25,d100,la,lo);
   //Serial.println(txtmsg);
-  sms.print(txtmsg);
-  sms.endSMS();
-}
+ }
