c548184c7badab96c6dee74c6dabc1044279e37c
Adam Otto <aotto@ualberta.ca>
2018-02-14 15:35:17 -0700

    datainit now takes the header and body of the csv seperately.

--- ECE492GUI/scripts.js
+++ ECE492GUI/scripts.js
@@ -5,14 +5,14 @@
 $(document).ready(function() {
 	$.ajax({	
 		type: "GET",	
-		url: "test.csv",	
+		url: "test.csv",
 		dataType: "text",	
 		success: function(data) {csvFunction(data);}	
 	});
 });
 
 var csvData;
-var csvHeaders = [];
+var csvHeader = [];
 var csvContent = [];
 var map;
 //window.onscroll = function() {stickyFunc()};
@@ -26,8 +26,8 @@ function init(){
 	mapElement = document.getElementById("fixedDiv");
 	sticky = mapElement.offsetTop;
 	initMap();
-	initContent(csvData);
-	test(csvData);
+	parseData(csvData);
+	initContent(csvHeader, csvContent);
 	console.log("init end.");
 }
 
@@ -44,56 +44,55 @@ function initMap() {
 	//marker.setMap(map);
 }
 
-
-function initContent(data) {
+/**
+ * Reloads the table with data from the csv file based on the passed in value.
+ * 
+ * Takes in the headers of the table and the rows, or the body, of the table as arguments.
+ */
+function initContent(header, body) {
 	//Code help from -> https://code.tutsplus.com/tutorials/parsing-a-csv-file-with-javascript--cms-25626
 	console.log("initContent started...");
 	var htmlCode = "<table>";
-	var allRows = data.split(/\r?\n|\r/);
+	var hlat;
+	var hlon;
+	document.getElementById('content').innerHTML = "";
 	
-	for (var row = 0; row < allRows.length; row++) {
-		if (row === 0) {
-			htmlCode += '<thead>';
-			htmlCode += '<tr>';
-		}
-		else {
-			htmlCode += '<tr>';
-		}
+	htmlCode += '<thead><tr>'
+	for (var h = 0; h < header.length; h++) {
+		htmlCode += '<th>';
+		htmlCode += header[h];
+		htmlCode += '</th>';
 		
-		var rowCells = allRows[row].split(',');
+		if(header[h].toLowerCase().includes("latitude"))
+			hlat = h;
+		if(header[h].toLowerCase().includes("longitude"))
+			hlon = h;
+	}
+	htmlCode += '</tr>';
+	htmlCode += '</thead>';
+	htmlCode += '<tbody>';
+	
+	for (var row = 0; row < body.length; row++) {
+		htmlCode += '<tr>';
 		var latitude = 0;
 		var longitude = 0;
-		for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
-			if (row === 0) {
-				htmlCode += '<th>';
-				htmlCode += rowCells[rowCell];
-				htmlCode += '</th>';
-			} else {
-				if(row != 0){
-					if(rowCell == 1)
-						latitude = parseInt( rowCells[rowCell] );
-					if(rowCell == 2)
-						longitude = parseInt( rowCells[rowCell] );
-					}
-					htmlCode += '<td>';
-					htmlCode += rowCells[rowCell];
-					htmlCode += '</td>';
-			}
-		}
-		if(row != 0){
-			console.log("Adding marker " + row + ": Lat: " + latitude + ", Long: " + longitude);
-			var pos = new google.maps.LatLng(latitude, longitude);
-			var mark = new google.maps.Marker({position:pos});
-			mark.setMap(map);
-		}
 		
-		if (row === 0) {
-			htmlCode += '</tr>';
-			htmlCode += '</thead>';
-			htmlCode += '<tbody>';
-		} else {
-		  htmlCode += '</tr>';
+		for (var rowCell = 0; rowCell < body[row].length; rowCell++) {
+			if(rowCell == hlat)
+				latitude = parseInt( body[row][rowCell] );
+			if(rowCell == hlon)
+				longitude = parseInt( body[row][rowCell] );
+			htmlCode += '<td>';
+			htmlCode += body[row][rowCell];
+			htmlCode += '</td>';
 		}
+
+		console.log("Adding marker " + row + ": Lat: " + latitude + ", Long: " + longitude);
+		var pos = new google.maps.LatLng(latitude, longitude);
+		var mark = new google.maps.Marker({position:pos});
+		mark.setMap(map);
+		
+		htmlCode += '</tr>';
 	}
 	htmlCode += '</tbody>';
 	htmlCode += '</table>';
@@ -101,7 +100,7 @@ function initContent(data) {
 	console.log("initContent end.");
 }
 
-function test(data)
+function parseData(data)
 {
 	//var d1 = new Date(2017, 7, 24, 14, 52, 10);
 	//var d2 = Date.parse("2018 Feb 13 2:34:55");
@@ -119,8 +118,10 @@ function test(data)
 	filterTable("Tempurature", "-20", "<")
 }
 
-/*
+/**
  FilterTable
+ 
+ filters the contents of the csv file based on the arguments passed in.
  
  Arguments:  FilterTable takes in 3 strings as arguments.
  	filterKey: The header that we want to sort by. (This implementation will only allow simple, 1 value filtering.)
@@ -131,7 +132,7 @@ function test(data)
  			"<=" : filterValue is less than or equal to scvContent.
  			">" : filterValue is greater than scvContent.
  			"<" : filterValue is less than scvContent.
- * */
+*/
 function filterTable(filterKey, filterValue, filterType)
 {
 	var headerIndex = -1;
@@ -191,6 +192,16 @@ function filterTable(filterKey, filterValue, filterType)
 	}
 	
 	console.log(newTableContent);
+}
+
+/**
+ * 
+ * 
+ * Loads the latest data of the csv file.  Assumes the most recent data is appended to the end of the file.
+ */
+function LoadLatestData()
+{
+	
 }
 
 function csvFunction(data)
