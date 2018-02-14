205b3b5e638a52d0b4e8913df8b610f798994175
Alex Nguyen <ahnguyen@ualberta.ca>
2018-02-14 10:40:48 -0700

    Add files via upload

--- ECE492GUI/scripts.js
+++ ECE492GUI/scripts.js
@@ -17,22 +17,65 @@ function init(){
 	console.log("init end.");
 }
 
+function createContentString(station_number,temp,humididty,pmatter){
+	var contentString = '<h1 class= "contentstring"> REMOTE STATION' + station_number + '</h1>'+
+						'<p>Temperature: '+ temp + ' </p>'+
+						'<p>Humididty: '+ humididty + ' </p>' +
+						'<p>Particulate matter: ' + pmatter +' </p>';
+	return contentString;
+}
+
+
 function initMap() {
 	var center = new google.maps.LatLng(53.5444,-113.4909);
 	var mapProp= {
 	    center:new google.maps.LatLng(53.5444,-113.4909),
 	    zoom:5,
 	};
+	
 //	var marker = new google.maps.Marker({position:center});
+	
+//	var infoWindowContent = [
+//	['<h3> hello </h3>'],
+//	['<h2>sup</h2>']];
+	
 	
 	map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 	
-	var marker = new google.maps.Marker({
-      position: center,
-      map: map,
-      title: 'Click to zoom'
-        });
+	//var infoWindow = new google.maps.InfoWindow(),marker,i;
 	
+/*	var markers = [['Remote 1',53.5444,-113.4909],
+				   ['Remote 2',53.5444,-130.4909]];
+	int i 
+	
+	for(i = 0 ; i<markers.length;i++){
+		var position = new google.maps.LatLng(markers[i][1],markers[i][2]);
+		bounds.extend(position);
+		var marker = new google.maps.Marker({
+			position:position,
+			map:map,
+			title: markers[i][0]
+		});
+		
+	google.maps.event.addListener(marker,'click',(function(marker,i){
+		return function(){
+			infoWindow.setContent(infoWindowContent[i][0]);
+			infoWindow.open(map,marker);
+		}
+	}) (marker,i));
+	
+	map.fitBounds(bounds);
+
+	*////}	
+	
+		var marker = new google.maps.Marker({
+			position:center,
+			map:map,
+			title:'hello'
+		});
+
+		
+ 
 	map.addListener('center_changed', function() {
           // 3 seconds after the center of the map has changed, pan back to the
           // marker.
@@ -41,7 +84,12 @@ function initMap() {
           }, 3000);
         });
 	
-	 var contentString = 'remote station 1';
+	 var contentString = createContentString(1,190,20,39);
+	
+	
+	
+
+	
 	
 	var infowindow = new google.maps.InfoWindow({
     content: contentString
