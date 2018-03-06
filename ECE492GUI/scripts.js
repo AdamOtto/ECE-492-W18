2f9c9e138dddb274c3c634a3bd56ca4648bead3a
Adam Otto <aotto@ualberta.ca>
2018-03-05 21:32:33 -0700

    Got ajax calls to communicate with a php file on a local server.

--- ECE492GUI/scripts.js
+++ ECE492GUI/scripts.js
@@ -27,6 +27,7 @@ function init(){
 	sticky = mapElement.offsetTop;
 	initMap();
 	parseData(csvData);
+	//callServer();
 	initContent(csvHeader, csvContent);
 	console.log("init end.");
 }
@@ -309,4 +310,23 @@ window.onclick = function(event) {
       }
     }
   }
+}
+
+function callServer() {
+	
+	if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest({mozSystem: true});
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+            displayToLog(this.responseText);
+        }
+    };
+    xmlhttp.open("GET","http://localhost/serverCom.php",true);
+    xmlhttp.send();
 }
