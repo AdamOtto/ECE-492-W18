a75fa0d9897b0ec62c38e6e07318a34680f64c1b
Alex Nguyen <ahnguyen@ualberta.ca>
2018-03-16 15:23:51 -0600

    filtering now works with date filter

--- ECE492GUI/serverFilterPM.php
+++ ECE492GUI/serverFilterPM.php
@@ -6,24 +6,31 @@
 	$dbname = "ece492database";
 	/*$q = intval($_GET['q']);*/
 	// Create connection
+	$year = intval($_GET['year']);
+	$month = intval($_GET['month']);
+	$day = intval($_GET['day']);
+	$hour = intval($_GET['hour']);
+	$min = intval($_GET['min']);
+	$sec = intval($_GET['sec']);
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$q =  '"' . (string)$year . "-" . (string)$month . "-" . (string)$day . " " . (string)$hour . ":" . (string)$min . ":" . (string)$sec . '"';
 
 	// Check connection
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_error($conn));
 	}
 	mysqli_select_db($conn, "remotesensor");
-	$sql ="SELECT StationName,Dust,Date
+	$sql ="SELECT StationName,Latitude,Longitude,Dust,Date
 	FROM remotestation as r
-	WHERE r.Date = (SELECT MAX(Date) from remotestation as r2 where r2.StationName = r.StationName AND r2.Date < NOW())
+	WHERE r.Date = (SELECT MAX(Date) from remotestation as r2 where r2.StationName = r.StationName AND r2.Date < "  .$q.  ")
 	ORDER by r.StationName ASC;";
 	
 	$result = mysqli_query($conn, $sql);
 	$resultCheck = mysqli_num_rows($result);
-	echo "StationName,Dust,Date\n";
+	echo "StationName,Latitude,Longitude,Dust,Date\n";
 	if ($resultCheck > 0) {
 	    while($row = mysqli_fetch_assoc($result)) {
-	        echo $row['StationName'] . "," . $row["Dust"] . "," . $row["Date"] . "\n";
+	        echo $row['StationName'] ."," . $row["Latitude"] . "," . $row["Longitude"] ."," . $row["Dust"] . "," . $row["Date"] . "\n";
 	   }
 	}
 		else{
